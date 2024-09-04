21bdb05ea379a3c4c29e6a68f785310f7a0067be
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2024-09-05 01:42:10 +0900

    Change comment

--- src/simple_signal_slot.cpp
+++ src/simple_signal_slot.cpp
@@ -1,13 +1,34 @@
-#include "simple_signal_slot.h"
+﻿#include "simple_signal_slot.h"
 #include <stdio.h>
+
+/*===============================================================================================================*/
+/* Email:
+*		<nguyenthaithuanalg@gmail.com> - Nguyễn Thái Thuận
+* Mobile:
+*		<+084.799.324.179>
+* Skype:
+*		<nguyenthaithuanalg>
+* Date:
+*		<2024-Sep-04>
+* The lasted modified date:
+*		<2024-Sep-05>
+* Decription:
+*		TODO
+*/
+/*===============================================================================================================*/
 
 simple_signal_slot::simple_signal_slot()
 {
+	m_implement = 0;
 	fprintf(stdout, "---\n");
 }
 
 simple_signal_slot::~simple_signal_slot()
 {
+	if (m_implement) {
+		delete(m_implement);
+		m_implement = 0;
+	}
 	fprintf(stdout, "===========\n");
 }
 
