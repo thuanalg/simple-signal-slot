764be0eca156fac9122a464431dfee9e1c54f1d2
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2024-12-06 18:13:16 +0700

    test_linux

--- src/implement/simple_implement_signal_slot.cpp
+++ src/implement/simple_implement_signal_slot.cpp
@@ -45,9 +45,14 @@
 #define ss_pthread_mutex_unlock(__obj, __err) \
 		{ (__err) = pthread_mutex_unlock((pthread_mutex_t*)(__obj)); if((__err)) spllog(0, "pthread_mutex_unlock errcode: %d. %s\n", (__err), (__err) ? "FALIED": "DONE");}
 
-#define ss_sem_wait(__obj)				do { sem_wait((sem_t*)(__obj)) ;} while(0);
+//#define ss_sem_wait(__obj)				do { sem_wait((sem_t*)(__obj)) ;} while(0);
+//
+//#define ss_sem_post(__obj__)			do { sem_post((sem_t*)(__obj__)); } while(0);
 
-#define ss_sem_post(__obj__)			do { sem_post((sem_t*)(__obj__)); } while(0);
+#define SS_sem_wait(__obj) \
+		sem_wait((sem_t*)(__obj))
+#define SS_sem_post(__obj) \
+		sem_post((sem_t*)(__obj))
 #endif
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
